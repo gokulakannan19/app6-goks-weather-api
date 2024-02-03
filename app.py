88eccff79579305778f537b5da53b3e34a6cb43e
gokulakannan <gokulakannan.r@idp.com>
2024-02-03 12:25:02 +0530

    add url data for yearly and annual data

--- app.py
+++ app.py
@@ -18,12 +18,29 @@ def about(station, date):
     filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
-    print(temperature)
+
     return {
         "station": station,
         "date": date,
         "temperature": temperature
     }
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def get_yearly(station, year):
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))]
+    return result.to_dict(orient="records")
 
 
 if __name__ == "__main__":
